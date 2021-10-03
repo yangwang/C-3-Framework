eb1803696a8df552351fda937babb380a472728f
yangwang <49030340+yangwang@users.noreply.github.com>
2021-10-03 12:37:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ __C.NET = 'Res50' # net selection: MCNN, VGG, VGG_DECODER, Res50, CSRNet
 __C.PRE_GCC = False # use the pretrained model on GCC dataset
 __C.PRE_GCC_MODEL = '' # path to model
 
-__C.RESUME = False # contine training
-__C.RESUME_PATH = './exp/04-25_09-19_SHHB_VGG_1e-05/latest_state.pth' # 
+__C.RESUME = True # contine training
+__C.RESUME_PATH = './exp/10-02_16-30_SHHB_Res50_1e-05/latest_state.pth' # 
 
 __C.GPU_ID = [0] # sigle gpu: [0], [1] ...; multi gpus: [0,1]
 
@@ -35,7 +35,7 @@ __C.LR = 1e-5 # learning rate
 __C.LR_DECAY = 0.995 # decay rate
 __C.LR_DECAY_START = -1 # when training epoch is more than it, the learning rate will be begin to decay
 __C.NUM_EPOCH_LR_DECAY = 1 # decay frequency
-__C.MAX_EPOCH = 200
+__C.MAX_EPOCH = 300
 
 # print 
 __C.PRINT_FREQ = 10
